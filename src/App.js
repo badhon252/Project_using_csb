27a587df8605af1dccac964f84a6f2bba348b7ba
Khalid Hossain Badhon <khalidhossainbadhon@gmail.com>
2021-10-25 12:09:43 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <ul>
+        <li>JavaScript</li>
+        <li>Java</li>
+        <li>Python</li>
+        <li>C/C++</li>
+      </ul>
+    </div>
+  );
+}
